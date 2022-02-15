20a7523f1196d7656d02a8ce86410a0dc8f0ba97
Prasanna Sridharan <prasannas090801@gmail.com>
2022-02-15 20:50:15 +0530

    first commit

---
+++ client/src/components/shared/Author.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { link } from './helpers';
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
+
+const StyledLink = styled(Link)`
+  ${link};
+
+  font-weight: 500;
+  color: ${props => props.theme.normalText};
+`;
+
+const Author = ({ username }) => (
+
+    <StyledLink to={`/u/${username}`}>
+       
+     {username}
+      </StyledLink>
+
+);
+
+export default Author;
